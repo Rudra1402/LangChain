699a4c7cc31af83d0061132e7b28843c727fc6c5
Rudra Patel <78080396+Rudra1402@users.noreply.github.com>
2025-12-27 18:54:59 -0500

    feat(TavilySearch): add search functionality in the tool (#3)

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import os
 from dotenv import load_dotenv
+
 from langchain.agents import create_agent
 from langchain.tools import tool
+
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
+
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
 
 load_dotenv()
 
+tavily = TavilyClient()
 
+
+@tool
 def webSearch(query: str):
     """Search the web with the given query and return the raw result string."""
-    userInput = f"User Input - {query}"
+    userInput = f"Searching web for <{query}>"
     print(userInput)
-    return userInput
+    return tavily.search(query=query)
 
 
-llmModel = ChatOpenAI()
+llmModel = ChatOpenAI(model="gpt-5-nano")
 tools = [webSearch]
 # NOTE: create_agent() returns a runnable sequence like LCEL (LangChain Expression Language) using "|" pipe operator
 agent = create_agent(model=llmModel, tools=tools)
@@ -24,10 +31,11 @@ agent = create_agent(model=llmModel, tools=tools)
 
 def main():
     print("Hello from langchain-agents!")
+    query = "Capital of Spain?"
     res = agent.invoke({
-            "messages": HumanMessage(content="What is the capital of France?")
+            "messages": HumanMessage(content=query)
     })
-    print(res)
+    print(res["messages"][-1].content)
 
 
 if __name__ == "__main__":
