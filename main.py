dd1bb80ecd5f71d1bc99a6172e666ff9cfb73fc0
Rudra Patel <78080396+Rudra1402@users.noreply.github.com>
2025-12-28 17:38:58 -0500

    chore(ResponseFormat): add response format to agents response (#5)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+from typing import List
+from pydantic import BaseModel, Field
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 
@@ -16,8 +19,18 @@ load_dotenv()
 tavily = TavilyClient()
 tavilySearch = TavilySearch()
 
+class Source(BaseModel):
+    """URL source that was searched on the web"""
 
-# Replacing this with TavilySearch from langchain_tavily
+    url:str = Field(description="URL source that was searched on the web")
+
+class AgentResponse(BaseModel):
+    """Agents response to the query"""
+
+    answer:str = Field(description="Agents response to the query")
+    sources:List[Source] = Field(default_factory=List, description="List of URL sources")
+
+# Replacing this tool with TavilySearch from langchain_tavily when creating an agent
 @tool
 def webSearch(query: str):
     """Search the web with the given query and return the raw result string."""
@@ -29,12 +42,12 @@ def webSearch(query: str):
 llmModel = ChatOpenAI(model="gpt-5-nano", temperature=0)
 tools = [tavilySearch]
 # NOTE: create_agent() returns a runnable sequence like LCEL (LangChain Expression Language) using "|" pipe operator
-agent = create_agent(model=llmModel, tools=tools)
+agent = create_agent(model=llmModel, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-agents!")
-    query = "Weather in Spain?"
+    query = "Search 3 best and safest places near Canada to solo travel in 2026 with medium budget in CAD!"
     res = agent.invoke({
             "messages": HumanMessage(content=query)
     })
