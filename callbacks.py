64781cd727d86bb9cd34f3bbfedaa9d6f3f8aad3
Rudra Patel <78080396+Rudra1402@users.noreply.github.com>
2026-01-04 16:21:24 -0500

    chore(AgentActionLoop): execute AgentAction objects in while loop until AgentFinish object is returned (#13)

---
+++ callbacks.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any, List
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_core.outputs import LLMResult
+
+class AgentCallbackHandler(BaseCallbackHandler):
+    def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> Any:
+        """Run when LLM starts running."""
+        print(f"***** PROMPT to LLM *****\n{prompts[0]}")
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        """Run when LLM ends running."""
+        print(f"***** LLM Response *****\n{response.generations[0][0].text}")
