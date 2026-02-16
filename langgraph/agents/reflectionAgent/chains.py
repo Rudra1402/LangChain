9cd5111d29842a384b01b2f46a97f31da355fac1
Rudra Patel <78080396+Rudra1402@users.noreply.github.com>
2026-02-15 19:29:36 -0500

    feat(ReflectionAgent): Implement Reflection Agent Nodes and Edges (#23)

--- langgraph/agents/reflectionAgent/chains.py
+++ langgraph/agents/reflectionAgent/chains.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+
+load_dotenv()
 
 reflectionPrompt = ChatPromptTemplate.from_messages([
     (
@@ -16,7 +19,8 @@ generationPrompt = ChatPromptTemplate.from_messages([
         "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
         " Generate the best twitter post possible for the user's request."
         " If the user provides critique, respond with a revised version of your previous attempts.",
-    )
+    ),
+    MessagesPlaceholder(variable_name="messages")
 ])
 
 llm = ChatOpenAI()
--- langgraph/agents/reflectionAgent/main.py
+++ langgraph/agents/reflectionAgent/main.py
@@ -1,6 +1,58 @@
 from dotenv import load_dotenv
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
+from chains import generationChain, reflectionChain
 
 load_dotenv()
 
+REFLECT = "reflect"
+GENERATE = "generate"
+LAST = -1
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+def shouldReflect(state: MessageGraph):
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+
+def generationNode(state: MessageGraph):
+    return {"messages": [generationChain.invoke({"messages": state["messages"]})]}
+
+
+def reflectionNode(state: MessageGraph):
+    reflectionMsg = reflectionChain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=reflectionMsg.content)]}
+
+
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generationNode)
+builder.set_entry_point(GENERATE)
+builder.add_node(REFLECT, reflectionNode)
+builder.add_conditional_edges(GENERATE, shouldReflect, {
+    END: END,
+    REFLECT: REFLECT
+})
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+
 if __name__ == "__main__":
     print("Welcome to Reflection Agent!")
+    inputs = HumanMessage(content="""Make this tweet better:"
+                                @LangChainAI
+        — newly Tool Calling feature is seriously underrated.
+
+        After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+        Made a video covering their newest blog post
+
+        """
+    )
+    graphRes = graph.invoke({"messages": [inputs]})
+    print(graphRes)
